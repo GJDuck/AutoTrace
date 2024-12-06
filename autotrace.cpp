aa3b91d35c8068b1a5775b9acdbe7b587c7d8f8a
GJDuck <gregory@comp.nus.edu.sg>
2024-12-06 16:13:46 +0800

    Complete tool

--- autotrace.cpp
+++ autotrace.cpp
@@ -6,9 +6,33 @@
  * /_/   \_\__,_|\__\___/|_||_|  \__,_|\___\___|
  * 
  * Gregory J. Duck
+ *
+ * Copyright (C) National University of Singapore
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * The AutoTrace instrumentation module.
  */
 
 #include "stdlib.c"
+
+#include <stddef.h>
+#include <linux/prctl.h>
+#include <linux/seccomp.h>
+#include <linux/filter.h>
 
 static bool option_tty = false;
 
@@ -48,12 +72,6 @@ struct THREAD
     int id;					// Simplified thread ID
 };
 typedef struct THREAD THREAD;
-struct LOC
-{
-    const char *file;
-    size_t len;
-    unsigned line;
-};
 
 /*
  * Instrumentation "shared" state.
@@ -66,14 +84,12 @@ struct SHARED
     mutex_t lock;
     struct malloc_pool_s *pool;
     const char *progname;   // This program's name
-    LOC start;              // Start location
-    LOC stop;               // Stop location
     const char *dumpname;   // Dump file name
     void *threads;          // Threads
     int id;                 // Next thread id
     int dump;               // Full trace dump
-    bool disabled;          // Disabled?
     bool comma;             // Need comma?
+    bool tty;               // isatty?
 };
 
 /*
@@ -87,6 +103,61 @@ struct SHARED
  * Prototypes.
  */
 void fini(void);
+static void handler(int sig);
+
+/*
+ * Normalize a JSON string.
+ */
+static ssize_t normalize_string(const char *s, char t, char *buf, size_t size)
+{
+    if (s == NULL)
+        return 0;
+    bool done = false;
+    size_t k = 0;
+    for (size_t i = 0; !done && s[i] != '\0'; i++)
+    {
+        char c = s[i];
+        switch (c)
+        {
+            case '\"': case '\\':
+                if (k < size) buf[k++] = '\\';
+                if (k < size) buf[k++] = c;
+                break;
+            case '\t': case '\n': case '\r': case '\b': case '\f':
+                if (k < size) buf[k++] = '\\';
+                if (k < size)
+                {
+                    switch (c)
+                    {
+                        case '\t': buf[k++] = 't'; break;
+                        case '\n': buf[k++] = 'n'; break;
+                        case '\b': buf[k++] = 'b'; break;
+                        case '\f': buf[k++] = 'f'; break;
+                        case '\r': buf[k++] = 'r'; break;
+                    }
+                }
+                break;
+            default:
+                if ((uint8_t)c < 0x20 || (uint8_t)c > 0x7E)
+                {
+                    if (k < size) buf[k++] = '\\';
+                    if (k < size) buf[k++] = 'x';
+                    if (k < size) buf[k++] = '0';
+                    if (k < size) buf[k++] = '0';
+                    const char *X = "0123456789ABCDEF";
+                    if (k < size) buf[k++] = X[(uint8_t)c >> 4];
+                    if (k < size) buf[k++] = X[(uint8_t)c & 0x7];
+                }
+                else if (k < size)
+                    buf[k++] = c;
+                break;
+        }
+        done = (c == t);
+    }
+    if (k < size)
+        buf[k] = '\0';
+    return (ssize_t)k;
+}
 
 /*
  * Normalize a path.  DWARF paths may contain "../", "./", etc.
@@ -121,19 +192,10 @@ static ssize_t normalize_path(const char *path, char *buf, size_t size)
     }
     ptr = buf;
     for (size_t j = 0; j < i; j++)
-    {
-        const char *dir = dirs[j];
-        bool done = false;
-        for (size_t l = 0; !done && dir[l] != '\0'; l++)
-        {
-            if (k < size)
-                buf[k++] = dir[l];
-            done = (dir[l] == '/');
-        }
-    }
+        k += normalize_string(dirs[j], '/', buf + k, size - k);
     if (k < size)
-        buf[k++] = '\0';
-    return (ssize_t)(k-1);
+        buf[k] = '\0';
+    return (ssize_t)k;
 }
 
 /*
@@ -147,7 +209,15 @@ static int thread_compare(const void *a, const void *b)
 }
 static int thread_id(void)
 {
+    /*
+     * BUG: The following code assumes that tid's are never reused.  This is
+     *      almost always true for real programs, but not guaranteed.
+     */
+	static pid_t cached_tid = 0;
+    static int   cached_id  = 0;
     pid_t tid = gettid();
+	if (tid == cached_tid)
+        return cached_id;
     THREAD key;
     key.tid = tid;
 
@@ -165,42 +235,38 @@ static int thread_id(void)
             error("failed to create THREAD %d: %s", self->id, strerror(errno));
     }
     const THREAD *self = *(THREAD **)node;
+    cached_tid = self->tid;
+    cached_id  = self->id;
     return self->id;
-}
-
-/*
- * Match a location.
- */
-static bool match(const LOC *loc, const char *file, unsigned line)
-{
-    if (loc->line != line || loc->file == NULL)
-        return false;
-    size_t len = strlen(file);
-    if (len >= loc->len && strcmp(file + (len - loc->len), loc->file) == 0)
-        return true;
-    else
-        return false;
 }
 
 /*
  * Write an output entry.
  */
-static void output(const char *kind, int id, const char *file, unsigned line,
-    void *addr)
+static void output(const char *kind, int id, const char *func,
+    const char *file, unsigned line)
 {
     char buf[BUFSIZ];
     size_t size = sizeof(buf);
     ssize_t r = snprintf(buf, size, "%s\t{\"event\": \"%s\", \"thread\": %d, "
-        "\"file\": \"", (S->comma? ",\n": ""), kind, id);
+        "\"func\": \"", (S->comma? ",\n": ""), kind, id);
     if (r < 0 || (size_t)r >= size)
     {
         output_error:
-        error("failed to output event");
+        error("failed to output %s event", kind);
     }
+    r += normalize_string(func, '\0', buf+r, size-r);
+    if ((size_t)r >= size)
+        goto output_error;
+    ssize_t t = snprintf(buf+r, size-r, "\", \"file\": \"");
+    r += t;
+    if (t < 0 || (size_t)r >= size)
+        goto output_error;
+
     r += normalize_path(file, buf+r, size-r);
     if ((size_t)r >= size)
         goto output_error;
-    ssize_t t = snprintf(buf+r, size-r, "\", \"line\": %u}", line);
+    t = snprintf(buf+r, size-r, "\", \"line\": %u}", line);
     r += t;
     if (t < 0 || (size_t)r >= size)
         goto output_error;
@@ -211,91 +277,39 @@ static void output(const char *kind, int id, const char *file, unsigned line,
 /*
  * hit_line() is the main instrumentation entry point; called for every line.
  */
-void hit_line(const char *func, const char *file, unsigned line, void *addr)
+void hit_line(const char *func, const char *file, unsigned line)
 {
     if (mutex_lock(&S->lock) < 0)
         return;
-
-    if (S->disabled)
-    {
-        if (!match(&S->start, file, line))
-        {
-            mutex_unlock(&S->lock);
-            return;
-        }
-        S->disabled = false;
-    }
-
-    output("LINE", thread_id(), file, line, addr);
-
-    if (match(&S->stop, file, line))
-    {
-        // Artificial "crash":
-        fini();
-        exit(EXIT_STOP);
-    }
-
+    output("LINE", thread_id(), func, file, line);
     mutex_unlock(&S->lock);
 }
 
 /*
  * hit_call() is called for each function call.
  */
-void hit_call(const char *file, unsigned line, void *addr)
+void hit_call(const char *func, const char *file, unsigned line)
 {
     if (mutex_lock(&S->lock) < 0)
         return;
-
-    output("CALL", thread_id(), file, line, addr);
-
+    output("CALL", thread_id(), func, file, line);
     mutex_unlock(&S->lock);
 }
 
 /*
  * hit_return() is called for each return.
  */
-void hit_return(const char *file, unsigned line, void *addr)
+void hit_return(const char *func, const char *file, unsigned line)
 {
     if (mutex_lock(&S->lock) < 0)
         return;
-
-    output("RET ", thread_id(), file, line, addr);
-
+    output("RETURN", thread_id(), func, file, line);
     mutex_unlock(&S->lock);
 }
 
 /*
- * Parse a location (file:line)
+ * Called on entry.
  */
-static void parse_loc(const char *str, LOC *loc)
-{
-    char buf[BUFSIZ];
-    int colon = -1;
-    for (int i = 0; i < sizeof(buf)-1 && str[i] != '\0'; i++)
-    {
-        buf[i] = str[i];
-        colon = (str[i] == ':'? i: colon);
-    }
-    if (colon <= 0 || str[colon+1] == '\0')
-        error("failed to parse location \"%s\"; missing `:'", loc);
-    int val = atoi(str+colon+1);
-    if (val <= 0)
-        error("failed to parse location \"%s\"; inlocid line", loc);
-    loc->line = (unsigned)val;
-    buf[colon] = '\0';
-    loc->file = strdup(buf);
-    if (loc->file == NULL)
-        error("failed to duplicat filename: %s", strerror(errno));
-    loc->len = strlen(loc->file);
-}
-
-static void handler(int sig);
-
-#include <stddef.h>
-#include <linux/prctl.h>
-#include <linux/seccomp.h>
-#include <linux/filter.h>
-
 void init(int argc, char **argv, char **envp)
 {
     size_t size = MA_PAGE_SIZE;
@@ -308,8 +322,10 @@ void init(int argc, char **argv, char **envp)
         fprintf(stderr, "%s" NAME "%s: skip init\n", GREEN, OFF);
         // Already initialized by another binary:
         (void)munmap(ptr, size);
+        option_tty = S->tty;
         return;
     }
+    option_tty = S->tty = isatty(STDERR_FILENO);
     fprintf(stderr, "%s" NAME "%s: init\n", GREEN, OFF);
     S->pool = &malloc_pool;
 
@@ -325,23 +341,8 @@ void init(int argc, char **argv, char **envp)
 
     environ = envp;
 
-    const char *val = getenv("TRACE_START");
-    if (val != NULL)
-    {
-        S->disabled = true;
-        parse_loc(val, &S->start);
-    }
-
-    val = getenv("TRACE_STOP");
-    if (val != NULL)
-        parse_loc(val, &S->stop);
-
-    val = getenv("TRACE_DUMP");
+    const char *val = getenv("TRACE_DUMP");
     S->dumpname = (val == NULL? "TRACE.json.gz": val);
-//    S->dump = open(S->dumpname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-//    if (S->dump < 0)
-//        error("failed to open \"%s\" for writing: %s", S->dumpname,
-//            strerror(errno));
 
     // Fork-off a gzip process for compression:
     int fds[2];
@@ -357,17 +358,18 @@ void init(int argc, char **argv, char **envp)
         if (fd < 0)
             error("failed to open \"%s\" for writing: %s", S->dumpname,
                 strerror(errno));
-        dup(fd, STDOUT_FILENO);
+        dup2(fd, STDOUT_FILENO);
         close(fd);
-        dup(fds[0], STDIN_FILENO);
+        dup2(fds[0], STDIN_FILENO);
         close(fds[0]);
-        const char *argv[] = {"gzip", "--stdout", NULL};
-        execve("/bin/gzip", argv, environ);
+        const char * const argv[] = {"gzip", "--stdout", NULL};
+        execve("/bin/gzip", (char * const *)argv, environ);
         error("failed to execute `gzip' command: %s", strerror(errno));
     }
     close(fds[0]);
     S->dump = fds[1];
-    fputs("[\n", S->dump);
+    const char begin[] = "[\n";
+    write(S->dump, begin, sizeof(begin)-1);
     S->comma = false;
 
     // Make sure crashes call fini()
@@ -403,18 +405,24 @@ void init(int argc, char **argv, char **envp)
     }
 }
 
+/*
+ * Called on exit.
+ */
 void fini(void)
 {
     if (S->dump <= 0)
         return;
-    const char *end[] = "\n]\n";
+    const char end[] = "\n]\n";
     write(S->dump, end, sizeof(end)-1);
     close(S->dump);
     fprintf(stderr, "%s" NAME "%s: write dump \"%s\"...\n",
         GREEN, OFF, S->dumpname);
 }
 
-void quit(int code)
+/*
+ * Called on fast _exit().
+ */
+void hit_quit(int code)
 {
     // Make sure _exit() and _Exit() call fini()
     fini();
@@ -422,6 +430,9 @@ void quit(int code)
     abort();
 }
 
+/*
+ * Called on signal.
+ */
 static void handler(int sig)
 {
     fini();
